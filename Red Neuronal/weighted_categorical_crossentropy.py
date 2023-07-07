38009184fa6682e43fd8ed939325111816afef6f
Francisco-Jose-Guillen-Navarro <138373396+Francisco-Jose-Guillen-Navarro@users.noreply.github.com>
2023-07-07 18:28:54 +0200

    Update weighted_categorical_crossentropy.py

--- Red Neuronal/weighted_categorical_crossentropy.py
+++ Red Neuronal/weighted_categorical_crossentropy.py
@@ -5,8 +5,8 @@ def WeightedCategoricalCrossentropy(class_weight):
 
         # Convertimos los objetivos a una forma compatible con la 
         # función de pérdida.
-        # y_true = K.cast(y_true, 'int32')
-        # y_true = K.one_hot(y_true, num_classes=3)
+        y_true = K.cast(y_true, 'int32')
+        y_true = K.one_hot(y_true, num_classes=3)
         
         # Calculamos la pérdida comparando los valores reales con
         # los predichos.
